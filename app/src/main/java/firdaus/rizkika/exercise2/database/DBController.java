a4c2daf0f04808553a61acbb4a0bb7cf4c092243
Lalu Muhammad Rizkika Firdaus <iki070899@gmail.com>
2021-04-29 19:24:29 +0800

    Initial commit

---
+++ app/src/main/java/firdaus/rizkika/exercise2/database/DBController.java
@@ -0,0 +1,16 @@
+package firdaus.rizkika.exercise2.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBController extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
